9c500e9d4f114f42666573af5971f163ca02a9c0
E2251-Selman <Selmanayyildiz047@gmail.com>
2020-12-11 22:32:56 +0100

    Add Navbar from material iu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 // import './App.css';
 import Signup from "./pages/Signup"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div className="App">
       {/* <h1>Main Page</h1> */}
+      <Navbar/>
       <Signup/>
       {/* 
       Rouer
